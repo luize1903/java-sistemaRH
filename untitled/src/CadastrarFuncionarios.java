96e88e93d8f764febbd77a5e48fc3fcd236840da
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-01 20:59:20 -0300

    Aula6

---
+++ untitled/src/CadastrarFuncionarios.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+import java.util.Scanner;
+import java.util.String;
+
+
+public class CadastrarFuncionarios {
+    String nome;
+    String endereco;
+    String telefone;
+    Date dataAdmissao;
+    String cargo;
+    double salarioInicial;
+
+    public void cadastrar(Scanner entrada){
+
+    }
+
+}
---
+++ untitled/src/CargosComissionais.java
@@ -0,0 +1,5 @@
+public class CargosComissionais extends CadastrarFuncionarios{
+    double partLucros;
+    double qtdQuotas;
+
+}
